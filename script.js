1870bb9e206ec8e6c281d0d7fc8bb63cbf4de2cc
Gerren Keith <gerren.keith23@gmail.com>
2025-04-09 12:29:03 -0500

    Redid the layout and functionality
    deleted elements in the slide container so they can be updated dynamically
    each slide will be generated in the script.js file based on the info
    given in the array to be displayed using that info. function unfinished.

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slides = [
+//   {
+//     title: "Cave Of Forgotten Dreams",
+//     image: "img1.jpg"
+//   },
+//   {
+//     title: "The Mountain in the Infinite distance",
+//     image: "img2.jpg"
+//   },
+//   {
+//     title: "Whispers of the Ancients",
+//     image: "img3.jpg"
+//   }
+// ]
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const mySlides = [];
+let index = 0;
+
+function Slides(title, image) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.image = image;
+  this.sayTitle = function() {
+    console.log(this.title);
+  };
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function addSlide(title, image){
+  const newSlide = new Slides(title, image);
+  mySlides.push(newSlide);
+
+  newSlide.sayTitle();
 }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+addSlide("Cave Of Forgotten Dreams", "img1.jpg");
+addSlide("The Mountain in the Infinite distance", "img2.jpg");
+addSlide("Whispers of the Ancients", "img3.jpg");
+
+console.log(mySlides);
+
+function displaySlides(){
+  const slideContainer = document.getElementById('slideContainer');
+  slideContainer.innerHTML = " ";
+
+    const slideElement = document.createElement("div")
 }
