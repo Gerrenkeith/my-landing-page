e59757bc90fe35566f6813a83172f1e5220b094b
Gerren Keith <gerren.keith23@gmail.com>
2025-04-24 23:43:31 -0500

    SlideShow
    added a slideshow feature to the website to display images in a carousel format
    for better user experience.

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
-// let slides = [
-//   {
-//     title: "Cave Of Forgotten Dreams",
-//     image: "img1.jpg"
-//   },
-//   {
-//     title: "The Mountain in the Infinite distance",
-//     image: "img2.jpg"
-//   },
-//   {
-//     title: "Whispers of the Ancients",
-//     image: "img3.jpg"
-//   }
-// ]
+const slidesArray = ["img1.jpg", "img2.jpg", "img3.jpg"];
+let currentSlide = 0;
+let numOfSlideChanges = 0
 
-const mySlides = [];
-let index = 0;
+window.onload = function() {
+  const slides = document.querySelector(".slides")      
+        slides.style.backgroundImage = `url(${slidesArray[currentSlide]})`;  
+        slides.classList.add("change")
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % slidesArray.length;
+    slides.style.backgroundImage = `url(${slidesArray[currentSlide]})`;
+    slides.classList.add("change");
+    console.log(`Current slide: ${currentSlide}`);
+    numOfSlideChanges += 1;
+    console.log(`Number of slide changes: ${numOfSlideChanges}`)
+    
+  }
+, 10000); 
 
-function Slides(title, image) {
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.image = image;
-  this.sayTitle = function() {
-    console.log(this.title);
-  };
 }
 
-function addSlide(title, image){
-  const newSlide = new Slides(title, image);
-  mySlides.push(newSlide);
-
-  newSlide.sayTitle();
-}
-
-addSlide("Cave Of Forgotten Dreams", "img1.jpg");
-addSlide("The Mountain in the Infinite distance", "img2.jpg");
-addSlide("Whispers of the Ancients", "img3.jpg");
-
-console.log(mySlides);
-
-function displaySlides(){
-  const slideContainer = document.getElementById('slideContainer');
-  slideContainer.innerHTML = " ";
-
-    const slideElement = document.createElement("div")
-}
