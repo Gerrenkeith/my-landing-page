27d8c4e4c2a2f16f58a1446d5fe0abd8484e680b
Gerren Keith <gerren.keith23@gmail.com>
2025-04-26 20:17:41 -0500

    Styling and JavaScript changes
    added a gradients to the left and bottom sides of the slideshow
    to give it a faded look that blends into the background

--- script.js
+++ script.js
@@ -2,20 +2,27 @@ const slidesArray = ["img1.jpg", "img2.jpg", "img3.jpg"];
 let currentSlide = 0;
 let numOfSlideChanges = 0
 
+const leftGradient = "linear-gradient(to right,rgba(245, 243, 243, 1) 1%, rgba(255, 255, 255, 0) 50% , rgba(255, 255, 255, 0) 100%)";
+const bottomGradient = "linear-gradient(to top,rgba(245, 243, 243, 1) 1%, rgba(255, 255, 255, 0) 50% , rgba(255, 255, 255, 0) 100%)";
 window.onload = function() {
-  const slides = document.querySelector(".slides")      
-        slides.style.backgroundImage = `url(${slidesArray[currentSlide]})`;  
+  const slides = document.querySelector(".slides")     
+        slides.style.backgroundImage = `${leftGradient}, ${bottomGradient}, url(${slidesArray[currentSlide]})`;  
         slides.classList.add("change")
-  setInterval(() => {
-    currentSlide = (currentSlide + 1) % slidesArray.length;
-    slides.style.backgroundImage = `url(${slidesArray[currentSlide]})`;
-    slides.classList.add("change");
-    console.log(`Current slide: ${currentSlide}`);
-    numOfSlideChanges += 1;
-    console.log(`Number of slide changes: ${numOfSlideChanges}`)
-    
-  }
-, 10000); 
+        
+ 
 
+const slideShowInterval = setInterval(() => {
+  currentSlide = (currentSlide + 1) % slidesArray.length;
+  slides.style.backgroundImage = `${leftGradient}, ${bottomGradient}, url(${slidesArray[currentSlide]})`;
+  slides.classList.add("change");
+  console.log(`Current slide: ${currentSlide}`);
+  numOfSlideChanges += 1;
+  console.log(`Number of slide changes: ${numOfSlideChanges}`)
+  
+
+}, 10000); 
+
+
+console.log(slideShowInterval)
 }
 
